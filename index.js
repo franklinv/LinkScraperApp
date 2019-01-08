b5215130d348a75b4eb7d624650d6ad2d64d5a40
franklinv <franklin.vincent@gmail.com>
2019-01-08 22:57:50 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var baseurl=process.argv[2];
-// validation to check if the user entered a proper URL
-// in the second argument i.e args[2]
+var baseurl = process.argv[2]
+
 if (!IsValidUrl(baseurl))
 	{
 		console.log("Please enter a valid URL.");
 		return;
 	}
 
-
 var linkCount=parseInt(process.argv[3]);
-// validation to check if the user entered a number greater than 0
-// in the third argument i.e args[3]
-if (!(linkCount>0))
+
+
+
+
+if (isNaN(linkCount))
 {
-	console.log("Please enter a valid number greater than zero");
+	console.log("Please enter a valid number");
 	return;
 }
-
-
 var linkindex=0;
 getLinks(baseurl);
 
-
-//main function which recursively loads the html of each page 
-//and subpage url and retrieves the links
 function getLinks(url)
 {
 	
@@ -39,24 +34,30 @@ request(url, function(err, resp, body){
   links = $("a"); // get all hyperlinks
   $(links).each(function(i, link){
 	   //if the number of links displayed is equal to the
-	   // number of links to be displayed then exit
+	   // count of links specified in the args[2] then exit
 	   if (linkindex==linkCount)
 	   {
 		   return;
 	   }
-	 if ($(link).text().length>0)
+	  var objLink=$(link);
+	 if (objLink.text().length>0) 
 	 {
-			if ($(link).attr('href').indexOf("://")>=0)
+			if (objLink.attr('href').indexOf("://")>=0) 
 			{
-				console.log($(link).text() + '---->' + $(link).attr('href'));
+			   if (objLink.attr('href').indexOf(baseurl)>=0)
+				{
+				console.log(objLink.text() + '---->' + objLink.attr('href'));
+				linkindex++; 
+				}
 			}
 			else
 			{
-				console.log($(link).text() + '---->'+ baseurl + $(link).attr('href'));
+				console.log(objLink.text() + '---->'+ baseurl + objLink.attr('href'));
+				linkindex++; 
 			}
 	  
-	   linkindex++; 
-	   getLinks(baseurl + $(link).attr('href'));
+	   
+	   getLinks(baseurl + objLink.attr('href'));
      }
   });
   }
@@ -64,12 +65,9 @@ request(url, function(err, resp, body){
 
 }
 
-//function which validates whether the
-// Url is valid
+
 function IsValidUrl(strUrl) {
   return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(strUrl);
 }
-
-
 
 
